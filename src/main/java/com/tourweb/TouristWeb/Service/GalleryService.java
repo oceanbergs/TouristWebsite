41262921749bcafe63ec8fc88d2f500eb5c8a57e
RahulNimbokar101 <nimbokarg.rahul.com>
2025-01-19 12:59:53 +0530

    booking API

---
+++ src/main/java/com/tourweb/TouristWeb/Service/GalleryService.java
@@ -0,0 +1,7 @@
+package com.tourweb.TouristWeb.Service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface GalleryService {
+public String uploadfile(MultipartFile file);
+}
